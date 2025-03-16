f0e5e35c51c433234a20ec1c444e3e440df27b7a
ikeyu0806 <ikeyu0806@gmail.com>
2025-03-16 16:38:42 +0900

    enable get multi channels

--- lib/video_player_web/controllers/video_controller.ex
+++ lib/video_player_web/controllers/video_controller.ex
@@ -2,10 +2,20 @@ defmodule VideoPlayerWeb.VideoController do
   use VideoPlayerWeb, :controller
 
   def index(conn, _params) do
-    channel_id = "UCL6JY2DXJNDOIqCP1CRADng"  # チャンネルIDを設定
-    case VideoPlayer.Youtube.get_channel_videos(channel_id) do
-      {:ok, videos} ->
-        render(conn, :index, videos: videos)
+    channel_ids = [
+      "UCL6JY2DXJNDOIqCP1CRADng",
+      "UCaak9sggUeIBPOd8iK_BXcQ"
+    ]
+    videos = channel_ids
+      |> Enum.map(&VideoPlayer.Youtube.get_channel_videos/1)
+      |> Enum.reduce({:ok, []}, fn
+        {:ok, vids}, {:ok, acc} -> {:ok, acc ++ vids}
+        {:error, msg}, _ -> {:error, msg}
+      end)
+
+    case videos do
+      {:ok, all_videos} ->
+        render(conn, :index, videos: all_videos)
 
       {:error, message} ->
         render(conn, :index, error: message)
